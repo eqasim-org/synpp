7e3c342d4913961b2c4b5d2ee359884c7e664cdf
Sebastian Hörl <hoerl.sebastian@gmail.com>
2019-12-08 22:22:58 +0100

    Set up package structure and CI

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "README.md"), encoding = "utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name = "synpp",
+    version = "1.0.0",
+    description = "Synthetic population pipeline package for eqasim",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/eqasim-org/synpp",
+    author = "Sebastian Hörl",
+    author_email = "hoerl.sebastian@gmail.com",
+    keywords = "pipeline automation synthetic population dependency management transport",
+    package_dir = { "": "src" },
+    packages = find_packages(where = "src"),
+    python_requires='>=3.0',
+    install_requires = ["networkx>=2.4"],
+    extras_require = {
+        "test": ["pytest>=5.3.1"],
+    }
+)
