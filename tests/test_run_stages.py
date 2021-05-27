723dd394a6682642bd2bb9f4959f024b9f893d0d
u229351 <28100741+davibicudo@users.noreply.github.com>
2021-05-27 10:54:17 +0200

    add a couple of tests

--- tests/test_run_stages.py
+++ tests/test_run_stages.py
@@ -48,3 +48,25 @@ def test_cache_path(tmpdir):
     }], working_directory = tmpdir.mkdir("sub"))
 
     assert result[0] == "abc_uvw"
+
+def test_rerun_required(tmpdir):
+    working_directory = tmpdir.mkdir("sub")
+
+    results1 = synpp.run([{
+        "descriptor": "tests.fixtures.sum_config",
+        "config": {"a": 5, "b": 9}
+    }], working_directory=working_directory)
+    results2 = synpp.run([{
+        "descriptor": "tests.fixtures.sum_config",
+        "config": {"a": 5, "b": 9},
+    }], working_directory=working_directory, rerun_required=False, verbose=True)
+    assert results2['results'][0] == results1[0]
+    assert len(results2['stale']) == 0
+
+def test_wrapper(tmpdir):
+    working_directory = tmpdir.mkdir("sub")
+    wrapper = synpp.Synpp(config={'working_directory': working_directory})
+    assert 14 == wrapper.run_single(descriptor="tests.fixtures.sum_config", config={"a": 5, "b": 9})
+    res = wrapper.run_pipeline(definitions=[{"descriptor": "tests.fixtures.sum_config", "config": {"a": 5, "b": 9}}])
+    assert len(res) == 1
+    assert 14 == res[0]
