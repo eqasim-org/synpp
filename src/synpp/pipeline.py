d07c9a00e67588a007a8e53fc1c0c72796f79978
Aina <aina.rasoldier@hotmail.fr>
2023-01-21 11:51:22 +0100

    fix key type

--- src/synpp/pipeline.py
+++ src/synpp/pipeline.py
@@ -38,7 +38,7 @@ def rmtree(path):
 def flatten(d, parent_key='', sep='.'):
     items = []
     for k, v in d.items():
-        new_key = parent_key + sep + k if parent_key else k
+        new_key = str(parent_key) + sep + str(k) if parent_key else str(k)
         if isinstance(v, MutableMapping):
             items.extend(flatten(v, new_key, sep=sep).items())
         else:
