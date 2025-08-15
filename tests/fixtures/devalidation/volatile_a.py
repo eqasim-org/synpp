3623dc09dbeb42112dac87c1e7bdd31ee30baf9b
Sebastian Hörl <hoerl.sebastian@gmail.com>
2025-08-15 13:46:09 +0200

    fix: propagation of volatile options (#89)

--- tests/fixtures/devalidation/volatile_a.py
+++ tests/fixtures/devalidation/volatile_a.py
@@ -1,6 +1,7 @@
 def configure(context):
     context.config("a", volatile = True, default = 100)
     context.config("u.v.w", volatile = True, default = 0)
+    context.config("xyz")
 
 def execute(context):
-    return context.config("a") + context.config("u.v.w")
+    return context.config("a") + context.config("u.v.w") + context.config("xyz")
